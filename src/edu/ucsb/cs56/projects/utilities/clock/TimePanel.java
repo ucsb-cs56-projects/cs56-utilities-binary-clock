7f30108c4c520dcc09df187042120417bd6ba085
Daniel Shu <danielshu98@gmail.com>
2017-11-20 18:10:57 -0800

    WIP Refactoring TimePanel.java

--- src/edu/ucsb/cs56/projects/utilities/clock/TimePanel.java
+++ src/edu/ucsb/cs56/projects/utilities/clock/TimePanel.java
@@ -24,12 +24,16 @@ public class TimePanel extends JPanel
     protected GroupLayout layout;
     
 
-    protected Block s1, s2, s4, s8, s10, s20, s40;
-    ArrayList<Block> sec = new ArrayList<Block>();
-    protected Block m1, m2, m4, m8, m10, m20, m40;
-    ArrayList<Block> minute = new ArrayList<Block>();
-    protected Block h1, h2, h4, h8;
-    ArrayList<Block> hour = new ArrayList<Block>();
+    // protected Block s1, s2, s4, s8, s10, s20, s40;
+    // protected ArrayList<Block> sec1 = new ArrayList<Block>();
+    // protected ArrayList<Block> sec10 = new ArrayList<Block>();
+    // protected Block m1, m2, m4, m8, m10, m20, m40;
+    // protected ArrayList<Block> minute1 = new ArrayList<Block>();
+    // protected ArrayList<Block> minute10 = new ArrayList<Block>();
+    // protected Block h1, h2, h4, h8;
+    // protected ArrayList<Block> hour = new ArrayList<Block>();
+
+    protected ArrayList<ArrayList<Block>> timeBlocks = new ArrayList<ArrayList<Block>>();
 						
     protected Block PM, AM;
 
@@ -55,25 +59,51 @@ public class TimePanel extends JPanel
     */
     protected void initBlocks(Color onBlockColor_, Color offBlockColor_)
     {
-	String [] seconds = {"s1","s2","s4","s8","s10","s20","s40"};
-	for (String s: seconds){
-	    sec.add(new Block(onBlockColor_,  offBlockColor_));
+	String [] seconds1 = {"s1","s2","s4","s8"};
+    ArrayList<Block> sec1 = new ArrayList<Block>();
+	for (String s: seconds1){
+	    sec1.add(new Block(onBlockColor_,  offBlockColor_));
         }
 
-	String [] minutes = {"m1", "m2", "m4", "m8", "m10", "m20", "m40"};
-	for (String m: minutes){
-            minute.add(new Block(onBlockColor_,  offBlockColor_));
+    ArrayList<Block> sec10 = new ArrayList<Block>();
+    String [] seconds10 = {"s10","s20","s40"};
+    for (String s: seconds10){
+        sec10.add(new Block(onBlockColor_,  offBlockColor_));
+        }
+
+    ArrayList<Block> minute1 = new ArrayList<Block>();
+	String [] minutes1 = {"m1", "m2", "m4", "m8"};
+	for (String m: minutes1){
+            minute1.add(new Block(onBlockColor_,  offBlockColor_));
 
         }
 
+    ArrayList<Block> minute10 = new ArrayList<Block>();
+    String [] minutes10 = {"m10", "m20", "m40"};
+    for (String m: minutes10){
+            minute10.add(new Block(onBlockColor_,  offBlockColor_));
+
+        }
+
+    ArrayList<Block> hour = new ArrayList<Block>();
 	String [] hr = {"h1", "h2", "h4", "h8"};
 	for (String h: hr){
             hour.add(new Block(onBlockColor_,  offBlockColor_));
 
         }
-   
-	AM = new Block(onBlockColor_, offBlockColor_);
-        PM = new Block(onBlockColor_, offBlockColor_);
+    ArrayList<Block> ampm = new ArrayList<Block>();
+    String [] ap = {"AM", "PM"}
+    for (String a: ap){
+            ampm.add(new Block(onBlockColor_,  offBlockColor_));
+
+        }
+	
+    timeBlocks.add(ampm);
+    timeBlocks.add(sec1);
+    timeBlocks.add(sec10);
+    timeBlocks.add(minute1);
+    timeBlocks.add(minute10);
+    timeBlocks.add(hour);
 
     }
     /**
@@ -94,91 +124,89 @@ public class TimePanel extends JPanel
     }
     protected void initTutorial()
     {
-        //add gaps between components and edges
-        layout.setAutoCreateGaps(true);
-        layout.setAutoCreateContainerGaps(true);
+       //add gaps between components and edges
+       layout.setAutoCreateGaps(true);
+       layout.setAutoCreateContainerGaps(true);
 
 	      
         //create the guide labels
-        JLabel AMLabel = new JLabel("AM");
-	createLabel(AMLabel);
+       JLabel AMLabel = new JLabel("AM");
+	   createLabel(AMLabel);
                     
 	    
-	JLabel PMLabel = new JLabel("PM");
-	createLabel(PMLabel);
+	   JLabel PMLabel = new JLabel("PM");
+	   createLabel(PMLabel);
 	        
         
-	JLabel extraLabel = new JLabel(" ");  //placeholder whitespace for leftmost column
-        createLabel(extraLabel);	
+	   JLabel extraLabel = new JLabel(" ");  //placeholder whitespace for leftmost column
+       createLabel(extraLabel);	
 	
-        JLabel HLabel = new JLabel("Hours");
-	createLabel(HLabel);
+       JLabel HLabel = new JLabel("Hours");
+	   createLabel(HLabel);
            	    
 	    
-        JLabel M10Label = new JLabel("Minutes 10's");
-	createLabel(M10Label);
+       JLabel M10Label = new JLabel("Minutes 10's");
+	   createLabel(M10Label);
     	    
             	    
-        JLabel M1Label = new JLabel("Minutes 1's");
-	createLabel(M1Label);
+       JLabel M1Label = new JLabel("Minutes 1's");
+	   createLabel(M1Label);
             
                         	    
-        JLabel S10Label = new JLabel("Seconds 10's");
-	createLabel(S10Label);
+       JLabel S10Label = new JLabel("Seconds 10's");
+	   createLabel(S10Label);
                         
             	    
-	JLabel S1Label = new JLabel("Seconds 1's");
-	createLabel(S1Label);
+	   JLabel S1Label = new JLabel("Seconds 1's");
+	   createLabel(S1Label);
                         	    
-	JLabel N8Label = new JLabel("8"); //8
-	createLabel(N8Label);
+	   JLabel N8Label = new JLabel("8"); //8
+	   createLabel(N8Label);
             
-        JLabel N4Label = new JLabel("4"); //4
-	createLabel(N4Label);
+       JLabel N4Label = new JLabel("4"); //4
+	   createLabel(N4Label);
             
-        JLabel N2Label = new JLabel("2"); //2
-	createLabel(N2Label);
+       JLabel N2Label = new JLabel("2"); //2
+	   createLabel(N2Label);
             
-        JLabel N1Label = new JLabel("1");
-	createLabel(N1Label);
-	    
+       JLabel N1Label = new JLabel("1");
+	   createLabel(N1Label);
+	       
         //tell the layout how to set up columns
-        GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+       GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
 
-        hGroup.addGroup(layout.createParallelGroup().
-             addComponent(AMLabel).
-             addComponent(PMLabel));
-        hGroup.addGroup(layout.createParallelGroup().
-	     addComponent(extraLabel). //extra
-	     addComponent(AM ).
-             addComponent(PM ));
-	HLabel.addComponent(hour.get(3) );
-	HLabel.addComponent(hour.get(2));
-	HLabel.addComponent(hour.get(1));
-	HLabel.addComponent(hour.get(0));
-	hGroup.addGroup(layout.createParallelGroup().addComponent(HLabel));
-        hGroup.addGroup(layout.createParallelGroup().
-             addComponent(M10Label).
-             addComponent(m40).
-             addComponent(m20).
-             addComponent(m10));
-        hGroup.addGroup(layout.createParallelGroup().
-             addComponent(M1Label).
-             addComponent(m8 ).
-             addComponent(m4 ).
-             addComponent(m2 ).
-             addComponent(m1 ));
-        hGroup.addGroup(layout.createParallelGroup().
-             addComponent(S10Label).
-             addComponent(s40).
-             addComponent(s20).
-             addComponent(s10));
-        hGroup.addGroup(layout.createParallelGroup().
-             addComponent(S1Label).
-             addComponent(s8 ).
-             addComponent(s4 ).
-             addComponent(s2 ).
-             addComponent(s1 ));
+       hGroup.addGroup(layout.createParallelGroup().
+        addComponent(AMLabel).
+        addComponent(PMLabel));
+
+       GroupLayout.ParallelGroup ampmGroup = layout.createParallelGroup();
+       hGroup.addGroup(ampmGroup);
+       createParallelG(timeBlocks.get(0), ampmGroup, extraLabel);
+
+
+       GroupLayout.ParallelGroup hourGroup = layout.createParallelGroup();
+	   hGroup.addGroup(hourGroup);
+       createParallelG(timeBlocks.get(5), hourGroup, HLabel);
+
+	   hGroup.addGroup(layout.createParallelGroup().addComponent(HLabel));
+
+        GroupLayout.ParallelGroup minute10Group = layout.createParallelGroup();
+        hGroup.addGroup(minute10Group);
+        createParallelG(timeBlocks.get(4), minute10Group, M10Label);
+
+        GroupLayout.ParallelGroup minute1Group = layout.createParallelGroup();
+        hGroup.addGroup(minute1Group);
+        createParallelG(timeBlocks.get(3), minute1Group, M1Label);
+
+        GroupLayout.ParallelGroup second10Group = layout.createParallelGroup();
+        hGroup.addGroup(second10Group);
+        createParallelG(timeBlocks.get(2), second10Group, S10Label);
+
+        GroupLayout.ParallelGroup second1Group = layout.createParallelGroup();
+        hGroup.addGroup(second1Group);
+        createParallelG(timeBlocks.get(1), second1Group, S1Label);
+
+        
         hGroup.addGroup(layout.createParallelGroup().
             addComponent(N8Label).
             addComponent(N4Label).
@@ -195,6 +223,8 @@ public class TimePanel extends JPanel
 	     addComponent(M1Label).
 	     addComponent(S10Label).
 	     addComponent(S1Label));
+
+        GroupLayout.ParallelGroup eightGroup = layout.createParallelGroup(Alignment.BASELINE);
         vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).
 	    addComponent(N8Label).
             addComponent(h8 ).
@@ -228,12 +258,21 @@ public class TimePanel extends JPanel
         layout.setVerticalGroup(vGroup);
     }
 
+
+
+    public void createParallelG(ArrayList<Block> blocks, GroupLayout.ParallelGroup pGroup, JLabel label){
+        pGroup.addComponent(label);
+        for(Block b : blocks){
+            pGroup.addComponent(b);
+        }
+    }
     /**
         Get the blocks representing hours
         @return an array of Blocks h8, h4, h2, h1
     */
     public Block[] getHour()
     {
+
         Block[] b = {h1, h2, h4, h8};
         return b;
     }
